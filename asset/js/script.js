808fb8b99b78addea03d612a490aa8370cf909b1
Yiseo Kwon <yiseo1122@gmail.com>
2023-07-16 19:19:37 -0500

    feat: Display saved schedule lists

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,24 +1,76 @@
 // the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
-  // Display the current date in the header
+  // global variables
   let today = dayjs().format("MMMM DD, YYYY (dddd)");
+  let currentHours = dayjs().format("HH");
+  let timeBlockDiv;
+
+  // Display the current date in the header
   $("#currentDay").text(today);
 
+  // present time blocks for standard business hours of 9am to 5pm
+  function displayTimeBlocks(){
+    for(let hour = 9; hour <= 17; hour++){
+      timeBlockDiv = $("<div></div>").attr("id", "hour-" + hour).addClass("row time-block");
+      let timeBlockContents = 
+      `
+      <div class="col-2 col-md-1 hour text-center py-3">${hour > 12? hour + ":00 PM" : hour + ":00 AM"}</div>
+      <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+      <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+      `
+      
+      $("#timeBlockContainer").append(timeBlockDiv);
+      timeBlockDiv.append(timeBlockContents);
+
+      // The time block color changes depending on the current time.
+      if(currentHours > hour){  
+        timeBlockDiv.addClass("past");
+      }else if(currentHours == hour){
+        timeBlockDiv.addClass("present");
+      }else{
+        timeBlockDiv.addClass("future");
+      }
+    }
+  }
+  displayTimeBlocks();
+  
+
+
+  // Get schedule list from local storage
+  let storedObjects = JSON.parse(localStorage.getItem("schedule"));
+  if (!storedObjects){
+    storedObjects = [];
+  }
+
+  function getSavedSchedule(){  
+    $("#timeBlockContainer .time-block").each(function(){
+      let timeBlockId = $(this).attr("id");
+      for(let i = 0; i < storedObjects.length; i++){
+        let storedItemId = storedObjects[i].id;
+        if(timeBlockId == storedItemId){
+          $(this).find(".description").val(storedObjects[i].value);
+        }
+      }
+    });
+  }
+  getSavedSchedule();
+
+
+  // When the save button is clicked, values of textarea save to local storage
+  function handleSaveSchedule(){
+    let description = $(this).siblings(".description").val();
+    let descriptionBlockId = $(this).parent(".time-block").attr("id");
+    let descriptionObj = {
+      value: description,
+      id: descriptionBlockId
+    }
+
+    storedObjects.push(descriptionObj);
+    window.localStorage.setItem("schedule", JSON.stringify(storedObjects));
+  }
+  
+  // Event Listener
+  $("#timeBlockContainer").on("click", ".saveBtn", handleSaveSchedule);
 });
